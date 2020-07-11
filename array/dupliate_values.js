fa7f38c69839f66f1f7c1ce3b392e031a2a16aac
caleb <akejucaleb@yahoo.com>
2020-07-11 03:45:13 +0100

    initial commit

--- array/dupliate_values.js
+++ array/dupliate_values.js
@@ -3,8 +3,8 @@
 *
 * 		Print the dulicate values in an array?
 *
-* 	@param array
-*   @return array
+* 	@param arr
+*   @return {array}
 */
 
 function print_duplicate_values(arr)
@@ -13,14 +13,47 @@ function print_duplicate_values(arr)
 
 	return arr.filter( (e, i, a) => {
 
-			return a.indexOf(e) != i;
+			return a.indexOf(e) !== i;
 	})
 }
+
+/**
+*	Question:
+*
+* 		Remove the array value duplicate?
+*
+* 	@param arr
+*   @return {array}
+*/
+
+function remove_duplicate_values(arr)
+{
+	//e: values, i: index, a: array
+
+	return arr.filter( (e, i, a) => {
+
+			return a.indexOf(e) === i;
+	})
+}
+
+/**
+*	Question:
+*
+* 		Check if array contains duplicate value?
+*
+* 	@param arr
+*   @return {boolean}
+*/
+
+
+
 
 
 
 //usage
 
 print_duplicate_values([1,2,2,3,4,5,6,7,7,1]);
-
 //[2,7,1]
+remove_duplicate_value([1,2,2,3,4,5,6,7,7,1]);
+//[1,2,3,4,5,6,7]
+
