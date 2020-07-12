7bdc02dfb946e67977d95b147203dec15afbc22e
Karleb <surfersdevelopers@gmail.com>
2020-07-12 09:20:28 +0100

    Update dupliate_values.js

--- array/dupliate_values.js
+++ array/dupliate_values.js
@@ -4,7 +4,7 @@
 * 		Print duplicate value in an array of numbers and string??
 *
 * 	@param arr
-*   @return {array}
+*	@return {array}
 */
 
 function print_duplicate_values(arr)
@@ -23,7 +23,7 @@ function print_duplicate_values(arr)
 * 		Remove duplicate value in an array of numbers and string?
 *
 * 	@param arr
-*   @return {array}
+*	@return {array}
 */
 
 function remove_duplicate_values(arr)
@@ -39,24 +39,24 @@ function remove_duplicate_values(arr)
 /**
 *	Question:
 *
-* 	     Given a list of numbers, where every number shows up twice except for one number, 
-*        find that one number?
+* 	     	Given a list of numbers, where every number shows up twice except for one number, 
+*		find that one number?
 *
 * 	@param arr
-*   @return {number}
+*   	@return {number}
 */
 
-    function find_single_number(arr)
-    {
-        let singleNumber = 0;
-        let i = 0;
+function find_single_number(arr)
+{
+let singleNumber = 0;
+let i = 0;
 
-        for(i of arr){
-            singleNumber ^= i;
-        }
+for(i of arr){
+    singleNumber ^= i;
+}
 
-        return singleNumber;
-    }
+return singleNumber;
+}
 
 
 
